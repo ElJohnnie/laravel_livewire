1f9d5fc0dd6aa91a0dd760e070c6583dfe20bfb2
ElJohnnie <johnfollmer1995@gmail.com>
2021-06-02 21:26:58 -0300

    alteração layout

--- app/Models/User.php
+++ app/Models/User.php
@@ -28,6 +28,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'profile_photo_path'
     ];
 
     /**
@@ -66,5 +67,9 @@ class User extends Authenticatable
 
     public function likes(){
         return $this->hasMany(User::class);
+    }
+
+    public function getPhotoAttribute(){
+        return $this->profile_photo_path;
     }
 }
